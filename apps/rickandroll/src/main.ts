44f748849007ed7e3d1756739320427a8372c89d
Adam Azira <aladdintirta@gmail.com>
2021-11-29 18:56:15 +0700

    leverage

--- apps/rickandroll/src/main.ts
+++ apps/rickandroll/src/main.ts
@@ -8,7 +8,7 @@ import * as express from 'express';
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to rickandroll!' });
+  res.send({ message: 'Welcome to rickandroll! Yeah' });
 });
 
 const port = process.env.port || 3333;
